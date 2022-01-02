66f7451225e07985420dafd074fc049a637d26d8
Kara Thrash <>
2022-01-02 16:02:06 -0500

    back to basics

---
+++ Assets/_scripts/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public List<ShipSystem> systems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (systems == null)
+        { systems = new List<ShipSystem>(); }
+
+        foreach (Transform el in transform)
+        { 
+            if (el.GetComponent<ShipSystem>())
+            { 
+                systems.Add(el.GetComponent<ShipSystem>());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void Control(SystemType _system,bool _on) 
+    {
+    
+    }
+    public virtual void Control(SystemType _system,float _value) 
+    {
+        
+        foreach (ShipSystem el in systems)
+        {
+            if (el.SystemType() == _system)
+            { }
+        }
+    }
+
+
+}
---
+++ Assets/_scripts/ShipSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSystem : MonoBehaviour
+{
+    public SystemType systemType;
+
+    public Ship ship;
+
+    public float power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thruster();
+    }
+
+
+    public void Thruster()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * power * Time.deltaTime);
+    }
+
+
+
+
+
+    public SystemType SystemType() { return systemType; }
+    public void SystemType(SystemType _type) {  systemType = _type; }
+}
